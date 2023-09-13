4ecd676ba6576ce6a088160368e37de645ccec3b
Daniel Rhim <drhim@amazon.com>
2023-09-13 14:56:12 +0900

    Added updated examples for monad

--- src/HankukConsole/Program.fs
+++ src/HankukConsole/Program.fs
@@ -8,22 +8,37 @@ type ModelParam =
       seed: int
       gpuLayerCount: int }
 
+type Prompt =
+| Stop
+| Msg of string
+
+let convertToDU = function
+| "stop" -> Stop
+| msg -> Msg msg
+
+let convertToString = function
+| Msg m -> m
+| Stop -> "stop"
+
 let modelPath = "/Users/drhim/dev/HankukUniversityDemo/models/llama-2-7b-chat.Q4_0.gguf"
 
-let modelParam =
-    { modelPath = modelPath
-      contextSize = 1024
-      seed = 1337
-      gpuLayerCount = 2 }
+let createModelParams modelParam =
+    let modelParams =
+            new Common.ModelParams(modelParam.modelPath)
+    modelParams.ContextSize <- modelParam.contextSize
+    modelParams.Seed <- modelParam.seed
+    modelParams.GpuLayerCount <- modelParam.gpuLayerCount
+    modelParams
+    
 
 let rec body (session : ChatSession) prompt =
     match prompt with
-    | "stop" -> ()
-    | _ -> 
+    | Stop -> ()
+    | Msg msg -> 
         let inferenceParams = new InferenceParams()
         inferenceParams.Temperature <- 0.6f
         inferenceParams.AntiPrompts <- [ "User:" ]
-        let text = session.Chat(prompt, inferenceParams)
+        let text = session.Chat(msg, inferenceParams)
         
         text 
         |> Seq.map (fun x -> Console.Write(x)) 
@@ -31,22 +46,25 @@ let rec body (session : ChatSession) prompt =
         |> ignore 
         
         Console.ForegroundColor <- ConsoleColor.Green
-        let updatedPrompt = Console.ReadLine()
+        let updatedPrompt = Console.ReadLine() |> convertToDU
         Console.ForegroundColor <- ConsoleColor.White
 
         body session updatedPrompt
 
 [<EntryPoint>]
 let main argv =
-    let modelParams =
-        new Common.ModelParams(modelParam.modelPath)
-    modelParams.ContextSize <- modelParam.contextSize
-    modelParams.Seed <- modelParam.seed
-    modelParams.GpuLayerCount <- modelParam.gpuLayerCount
+
+    let modelParam = { 
+        modelPath = modelPath
+        contextSize = 1024
+        seed = 1337
+        gpuLayerCount = 5 }
+
+    let modelParams = createModelParams modelParam 
     
     use model = LLamaWeights.LoadFromFile(modelParams)
     use ctx = model.CreateContext(modelParams)
-    let prompt = "Hello! Feel free to ask me a question"
+    let prompt = Msg "Hello! Feel free to ask me a question" 
 
     let ex = new InteractiveExecutor(ctx)
     let transformer = new LLamaTransforms.KeywordTextOutputStreamTransform([| "Users:"; "AI"|], 8)    
@@ -55,7 +73,7 @@ let main argv =
     Console.ForegroundColor <- ConsoleColor.Yellow
     Console.WriteLine("The chat session has started. The role names won't be printed.")
     Console.ForegroundColor <- ConsoleColor.White;
-    Console.Write(prompt)
+    Console.Write(convertToString prompt)
     
     body session prompt
 
